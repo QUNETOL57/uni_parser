9289aeb833340c22e5ccc6bd3244bf881205cb7c
QUNETOL57 <qunetol57@outlook.com>
2021-09-18 19:44:20 +0300

    create base db structure

---
+++ base_structure_db.sql
@@ -0,0 +1,32 @@
+CREATE TABLE teacher (
+    id          INTEGER       PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name        VARCHAR (255) NOT NULL,
+    is_tutor    BOOLEAN       NOT NULL
+);
+
+CREATE TABLE type (
+    id          INTEGER       PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name        VARCHAR (255) NOT NULL
+);
+
+INSERT INTO type (id, name) VALUES (1, 'Семинары');
+INSERT INTO type (id, name) VALUES (2, 'Лабораторные работы');
+INSERT INTO type (id, name) VALUES (3, 'Контрольные работы');
+INSERT INTO type (id, name) VALUES (4, 'Курсовые работы');
+
+CREATE TABLE subject (
+    id          INTEGER       PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name        VARCHAR (255) NOT NULL,
+    semestr     INTEGER       NOT NULL,
+    type_id     INTEGER       REFERENCES type (id) ON DELETE CASCADE,
+    teacher_id  INTEGER       REFERENCES teacher (id) ON DELETE CASCADE
+);
+
+CREATE TABLE message (
+    id          INTEGER       PRIMARY KEY AUTOINCREMENT NOT NULL,
+    teacher_id  INTEGER       REFERENCES teacher (id) ON DELETE CASCADE,
+    subject_id  INTEGER       REFERENCES subject (id) ON DELETE CASCADE,
+    my_answer   BOOLEAN       NOT NULL,
+    text        TEXT          NOT NULL,
+    create_at   DATETIME      NOT NULL
+);
--- main.py
+++ main.py
@@ -2,44 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from dotenv import load_dotenv, find_dotenv
-import sqlite3
+
+import db
 
 load_dotenv(find_dotenv())
 
 MIREA_LOGIN=os.getenv('USER_LOGIN')
 MIREA_PASSWORD=os.getenv('USER_PASSWORD')
 MY_NAME=os.getenv('MY_NAME')
-PORTAL_NAME='https://portal.stavuniver.ru/'
+PORTAL_URL='https://portal.stavuniver.ru/'
 SEMESTR = 5
 
-
 session  = requests.Session()
-authorization = session.post(PORTAL_NAME, data = {'name':MIREA_LOGIN,'pass':MIREA_PASSWORD})
-# tutor = session.get('{PORTAL_NAME}mail_messages.php?action=messages&id=7741')
+authorization = session.post(PORTAL_URL, data = {'name':MIREA_LOGIN,'pass':MIREA_PASSWORD})
 
-# soup = BeautifulSoup(tutor.text, 'lxml')
-
-# dylog_table = soup.find("table", id="ramka2")
-
-# dylog_name = dylog_table.find('td', class_='main_head2').text
-
-# TUTOR_NAME = dylog_name.replace('Диалог с', '').strip()
-
-# for tr in dylog_table.find_all('tr')[2:]:
-# 	for td in tr.find_all('td'):
-# 		if TUTOR_NAME in td.text:
-# 			print("TUTOR - ", end='')
-# 		elif MY_NAME in td.text:
-# 			print("ME - ", end='')
-# 		else:
-# 			print(td.text.strip())
-# 			print('-'*30)
-
+sections = [BeautifulSoup(session.get(f'{PORTAL_URL}edu_process.php?work_type={i}').text, 'lxml') for i in range(1,5)]
 
 def get_educators():
-	# Список страниц с предметами 
-	sections = [BeautifulSoup(session.get(f'{PORTAL_NAME}edu_process.php?work_type={i}').text, 'lxml') for i in range(1,5)]
-
+	"""Список страниц с предметами"""
 	educators = []
 	for section in sections:
 		# title = section.find('h4', class_='zag').text.strip()
@@ -58,22 +38,25 @@ def get_educators():
 					])
 	return educators
 
+def set_teachers():
+	"""Загружает в базу список предподавателей"""
+	teachers = set()
+	for section in sections:
+		for line in section.find('table', class_='table-hover').find_all('tr')[1:]:
+			if line.find_all('td')[-1].find('a', class_='btn-danger'):
+				teacher_name = line.find_all('td')[-2].text
+				teachers.add(teacher_name)
 
+	teachers = list(map(lambda teacher: [teacher, False], teachers))
 
+	tutor = BeautifulSoup(session.get(f'{PORTAL_URL}mail_messages.php?action=messages&id=7741').text, 'lxml').find('td', class_='main_head2').text
+	tutor = tutor.replace('Диалог с', '').strip()
+	teachers.append([tutor, True])
 
-educators = get_educators()
+	for teacher in teachers:
+		db.insert('teacher', {
+			'name'		: teacher[0],
+			'is_tutor'	: teacher[1]
+		})
 
-def insert_educators(educators):
-	connect = sqlite3.connect('base.db')
-	cursor = connect.cursor()
-	cursor.execute("""CREATE TABLE IF NOT EXISTS educators (
-		id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-		name     VARCHAR (255) NOT NULL,
-		subject  VARCHAR (255) NOT NULL,
-		semestr  INTEGER (2)   NOT NULL,
-		is_tutor BOOLEAN       NOT NULL
-	)""")
-	cursor.executemany("INSERT INTO educators (name, subject, semestr, is_tutor) VALUES (?,?,?,?)", educators)
-	connect.commit()
-
-insert_educators(educators)
+set_teachers()
